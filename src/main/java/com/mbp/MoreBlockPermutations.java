1d4ffba149a6a8959838bf0866e512ffd7a28356
Matt Manning <mattmann.1299@gmail.com>
2025-06-22 14:27:21 -0500

    Initial commit

---
+++ src/main/java/com/mbp/MoreBlockPermutations.java
@@ -0,0 +1,16 @@
+package com.mbp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreBlockPermutations implements ModInitializer {
+	public static final String MOD_ID = "more-block-permutations";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/mbp/mbpClient.java
@@ -0,0 +1,10 @@
+package com.mbp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class mbpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
